d0972ac0d32e7a5cd2fc1712026055aa24b7dada
Quesia Mendes <quesmendes@gmail.com>
2023-02-10 19:40:31 -0300

    start implementation of chessboard

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QtdMovimentos { get; protected set; }  
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtdMovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }
+    }
+}
